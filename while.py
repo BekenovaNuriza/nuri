6ec4bb9dfd161da84878a6922790fc03b8edfe31
Nuriza <nurizabekenova53@gmail.com>
2025-02-11 15:30:15 +0600

    bekenova

---
+++ while.py
@@ -0,0 +1,20 @@
+# i=1
+# while i < 6:
+    
+#   print (i)
+
+# if i == 3:
+  
+#   breack
+
+# i += 1
+# for x in range(1,10,3):
+#     print(x)
+# 
+x = 5
+y = 1
+for x in range  (1 ,11):
+ for y in range (1 ,11):
+  print(f"{x} x {y} = {x*y}")
+  print()
+  
