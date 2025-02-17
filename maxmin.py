fd148fbda2a14a66acca9a7baa0f1e17979f7fa1
Nuriza <nurizabekenova53@gmail.com>
2025-02-17 16:39:20 +0600

    2

---
+++ maxmin.py
@@ -0,0 +1,12 @@
+number =[22, 45, 67, 89, 41, 90]
+max_numbers =[]
+min_numbers = []
+for x in numbers:
+    if x < min_number:
+        min_number = x
+    if x > max_number :
+        max_number = x
+print(f"наибольшое: {max_numbers}")
+print(f"наименьшее:{min_numbers}")
+
+
--- while.py
+++ while.py
@@ -11,10 +11,10 @@
 # for x in range(1,10,3):
 #     print(x)
 # 
-x = 5
-y = 1
-for x in range  (1 ,11):
- for y in range (1 ,11):
-  print(f"{x} x {y} = {x*y}")
-  print()
+# x = 5
+# y = 1
+# for x in range  (1 ,11):
+#  for y in range (1 ,11):
+#   print(f"{x} x {y} = {x*y}")
+#   print()
   
