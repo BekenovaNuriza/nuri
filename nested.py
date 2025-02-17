7900db5391e7be584d0d14237fea60242c43c52f
Nuriza <nurizabekenova53@gmail.com>
2025-02-17 16:39:13 +0600

    123

---
+++ nested.py
@@ -0,0 +1,27 @@
+# number = [10,20,0,40,50,61,60]
+# names = ["Akcholpon" , "Nur" ,]
+# number = [12, 7, 34, 23, 45, 66, 89, 90, 41, 55]
+
+# for x in number:
+#     print(x)
+
+# number =[22, 45, 67, 89, 41, 90]
+# max_numbers =[]
+# min_numbers = []
+# for x in numbers:
+#     if x < min_number:
+#         min_number = x
+#     if x > max_number :
+#         max_number = x
+# print(f"наибольшое: {max_numbers}")
+# print(f"наименьшее:{min_numbers}")
+
+    # def my function (food):
+    #     for x in food:
+    #         print(x)
+ = [12, 7, 34, 23, 45, 66, 89, 90, 41, 55]
+def evevfeven(args):
+    for i in args:
+        printnumber(i)
+evenfeven(number)
+    
